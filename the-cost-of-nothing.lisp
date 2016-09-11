7a6a72f23ae65112b208788682679f194f1774bf
Marco Heisig <marco.heisig@fau.de>
2016-09-11 11:06:05 +0200

    new and improved benchmarks for array allocation, hash tables, ...

--- the-cost-of-nothing.lisp
+++ the-cost-of-nothing.lisp
@@ -12,37 +12,103 @@
 (defun enlighten-me! ()
   (dolist (question (reverse *questions*))
     (format t "~a~%" question)
-    (funcall (symbol-function question))))
+    (funcall (symbol-function question))
+    (fresh-line)
+    (terpri)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; core functionality
+
+(answer |What is the cost of calling CONS?|
+  (print-time
+   (runtime
+    (lambda (invocations)
+      (let (tmp)
+        (dotimes (i invocations)
+          (push 42 tmp)))))))
+
+(answer |What is the cost of allocating arrays?|
+  (flet ((alloc-cost (bytes)
+           (runtime
+            (lambda (invocations)
+              (dotimes (i invocations)
+                (touch
+                 (make-array bytes :element-type '(unsigned-byte 8))))))))
+    (let* ((x0 4)
+           (x1 100000)
+           (y0 (alloc-cost x0))
+           (y1 (alloc-cost x1))
+           (slope (/ (- y1 y0) (- x1 x0))))
+      (format t "~a, plus ~a per byte allocated."
+              (print-time y0 nil)
+              (print-time slope nil)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; special forms
 
 (answer |What is the cost of calling a FLET?|
   (print-time
    (runtime
-     (lambda (invocations)
-       (flet ((f (x y) (eq x y)))
-         (dotimes (i invocations)
-           (f "foo" 5)))))))
+    (lambda (invocations)
+      (flet ((f (x y) (eq x y)))
+        (dotimes (i invocations)
+          (f "foo" 5)))))))
 
-(declaim (notinline binary-defun-stub))
-(defun binary-defun-stub (a b)
+(declaim (notinline binary-defun))
+(defun binary-defun (a b)
   (declare (ignore a b)))
 
-(answer |What is the cost of calling a binary DEFUN?|
+(answer |What is the cost of calling a DEFUN?|
   (print-time
    (runtime
-     (lambda (invocations)
-       (dotimes (i invocations)
-         (binary-defun-stub i i))))))
+    (lambda (invocations)
+      (dotimes (i invocations)
+        (binary-defun i i))))))
+
+(defmacro n-arg-defmethod (name n)
+  (let ((args (loop for i below n collect (gensym))))
+    `(defgeneric ,name (,@args)
+       (:method (,@args) (declare (ignore ,@args))))))
+
+(n-arg-defmethod 0-arg-defmethod 0)
+
+(n-arg-defmethod 50-arg-defmethod 50)
+
+(answer |What is the cost of calling a DEFMETHOD?|
+  (let* ((0-arg-cost
+           (runtime
+            (lambda (invocations)
+              (dotimes (i invocations)
+                (0-arg-defmethod)))))
+         (50-args (iota 50))
+         (50-arg-cost
+           (runtime
+            (lambda (invocations)
+              (dotimes (i invocations)
+                (apply #'50-arg-defmethod 50-args))))))
+    (let ((slope (/ (- 50-arg-cost 0-arg-cost) 50)))
+      (format t "~a plus ~a per argument.~%"
+              (print-time 0-arg-cost nil)
+              (print-time slope nil)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; list operations
 
 (answer |What is the cost of checking a single list element?|
   (print-time
    (runtime
-     (let ((l (make-circular-list 2 :initial-element 42)))
-       (lambda (invocations)
-         (dotimes (i invocations)
-           (when (/= (car l) 42) (return :?))
-           (setf l (cdr l))))))))
+    (let ((l (make-circular-list 2 :initial-element 42)))
+      (lambda (invocations)
+        (dotimes (i invocations)
+          (when (/= (car l) 42) (return :?))
+          (setf l (cdr l))))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;
+;;; hash tables
 
 (defun hash-runtime (&key test keys)
   (let ((hash-table (make-hash-table :test test))
@@ -50,31 +116,47 @@
     (loop for key across keys do
       (setf (gethash key hash-table) nil))
     (let ((keys (shuffle keys))
-          (size (length keys))
           tmp)
-      (runtime
-       (lambda (invocations)
-         (declare (type fixnum invocations)
-                  (type (simple-array t) keys))
-         (dotimes (i (floor invocations size))
-           (loop for key across keys do
-             (setf tmp (gethash key hash-table))))
-         tmp)))))
+      (values
+       (runtime
+        (lambda (invocations)
+          (declare (type fixnum invocations)
+                   (type (simple-array t (*)) keys))
+          (dotimes (i (floor invocations (length keys)))
+            (unroll (i (length keys) 8)
+              (setf tmp (gethash (svref keys i) hash-table))))))
+       tmp))))
 
-(answer |What is the cost of a EQL hash table lookup?|
-  (print-time
-     (hash-runtime :test #'eql :keys (iota 20))))
+(answer |What is the cost of a hash table lookup?|
+  (let ((eq (hash-runtime :test #'eq :keys (iota 40)))
+        (eql (hash-runtime :test #'eql :keys (iota 40)))
+        (equal-0 (hash-runtime :test #'equal :keys (iota 40)))
+        (equal-100
+          (let ((keys (loop for i below 40
+                            collect (make-list 100 :initial-element i))))
+            (hash-runtime :test #'equal
+                          :keys keys)))
+        (equalp-0 (hash-runtime :test #'equalp :keys (iota 40)))
+        (equalp-100
+          (let ((keys (loop for i below 40
+                            collect (make-list 100 :initial-element i))))
+            (hash-runtime :test #'equalp
+                          :keys keys))))
+    (format t "~a for an EQ hash table.~%"
+            (print-time eq nil))
+    (format t "~a for an EQL hash table.~%"
+            (print-time eql nil))
+    (format t "~a plus ~a per cons for an EQUAL hash table.~%"
+            (print-time equal-0 nil)
+            (print-time
+             (/ (- equal-100 equal-0) 100) nil))
+    (format t "~a plus ~a per cons for an EQUALP hash table.~%"
+            (print-time equalp-0 nil)
+            (print-time
+             (/ (- equalp-100 equalp-0) 100) nil))))
 
-(answer |What is the cost of a EQAL hash table lookup with a single cons?|
-  (let ((keys (loop for i below 20
-                    collect (cons i i))))
-    (print-time
-     (hash-runtime :test #'equal
-                   :keys keys))))
+;; TODO hash table versus alist
 
-(answer |What is the cost of a EQAL hash table lookup with ten conses?|
-  (let ((keys (loop for i below 20
-                    collect (list i (list (list i i) (list i i i))))))
-    (print-time
-     (hash-runtime :test #'equal
-                   :keys keys))))
+;; TODO hash table rehash cost
+
+;; TODO hash table rehash size
--- utilities.lisp
+++ utilities.lisp
@@ -5,12 +5,12 @@
 (defun runtime (f)
   (do ((invocations 1 (* 2 invocations)))
       ((> invocations (expt 2 30)) 0.0d0)
-    (let ((time (measure-runtime f invocations)))
+    (let ((time (funcall-time f invocations)))
       (when (> time 0.1)
         (return-from runtime (/ time invocations))))))
 
-(defun measure-runtime (function &rest arguments)
-  (gc)
+(defun funcall-time (function &rest arguments)
+  (gc) ; expensive, but crucial for reasonable results
   (let ((time (get-internal-real-time))
         result)
     (unwind-protect (apply function arguments)
@@ -19,13 +19,35 @@
                            'double-float)))
     result))
 
-(defun print-time (time)
+(defun print-time (time &optional (stream *standard-output*))
   (cond
-    ((< time 1.d-7)
-     (format t "> ~,2F nanoseconds~%" (* time 1.d9)))
-    ((< time 1.d-4)
-     (format t "> ~,2F microseconds~%" (* time 1.d6)))
-    ((< time 1.d-1)
-     (format t "> ~,2F milliseconds~%" (* time 1.d3)))
+    ((< time 1.d-6)
+     (format stream "~,2F nanoseconds" (* time 1.d9)))
+    ((< time 1.d-3)
+     (format stream "~,2F microseconds" (* time 1.d6)))
+    ((< time 1.d-0)
+     (format stream "~,2F milliseconds" (* time 1.d3)))
     (t
-     (format t "> ~,2F seconds~%" time))))
+     (format stream "~,2F seconds" time))))
+
+(defmacro unroll ((var n &optional (unroll 4)) &body body)
+  "Similar to DOTIMES."
+  (once-only (n)
+    (with-gensyms (remainder i)
+      `(let ((,remainder (rem ,n ,unroll)))
+         (do ((,i 0 (+ ,i ,unroll)))
+             ((>= ,i (- ,n ,remainder)))
+           (declare (type fixnum ,i))
+           ,@(loop for offset below unroll
+                   collect
+                   `(let ((,var (+ ,i ,offset)))
+                      ,@body)))
+         (do ((,var (- ,n ,remainder) (+ ,var 1)))
+             ((>= ,var ,n))
+           ,@body)))))
+
+(declaim (notinline touch))
+(defun touch (object)
+  "Essentially equivalent to IDENTITY, but prevents OBJECT from being
+  allocated on the stack of the caller."
+  object)
